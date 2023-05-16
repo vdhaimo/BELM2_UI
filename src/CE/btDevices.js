96d257f98c290e8609ed8c78b58a77f46312e4cc
vdhaimo <vishal.dhaimodkar22@gmail.com>
2023-05-16 21:20:37 +0530

    16.05.2023
    Ui updated

--- src/CE/btDevices.js
+++ src/CE/btDevices.js
@@ -9,7 +9,7 @@ class btListItem extends HTMLElement {
 
         let template = document.getElementById("tp-btdevlistitem");
 
-        const shadowRoot = this.attachShadow({ mode: "open" });
+        const shadowRoot = this.attachShadow({ mode: "closed" });
         const content = template.content.cloneNode(true);
 
 
@@ -104,8 +104,15 @@ class tpiconbutton extends HTMLElement {
             this.DESC.innerHTML = newValue
         }
         if (name === 'active') {
-            if (newValue === 'yes') this.MAIN.style.opacity = 1;
-            else this.MAIN.style.opacity = 0.5;
+            if (newValue === 'yes') {
+                this.MAIN.style.opacity = 1;
+                this.MAIN.style.color = getComputedStyle(this.MAIN).getPropertyValue('--c0');
+
+            }
+            else {
+                this.MAIN.style.opacity = 0.5;
+                this.MAIN.style.color = getComputedStyle(this.MAIN).getPropertyValue('--text-colorA');
+            }
         }
     }
 
@@ -113,6 +120,73 @@ class tpiconbutton extends HTMLElement {
 }
 
 window.customElements.define('bt-iconmatbutton', tpiconbutton, { mode: 'no-encapsulation' });
+
+
+
+
+
+class btCardX1 extends HTMLElement {
+
+    constructor() {
+        super();
+
+        const linkElem = document.createElement('link');
+        linkElem.setAttribute('rel', 'stylesheet');
+        linkElem.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,100,0,0');
+
+
+        let template = document.getElementById("tp-cardX1");
+
+        const shadowRoot = this.attachShadow({ mode: "closed" });
+        const content = template.content.cloneNode(true);
+
+
+
+        this.BIGTEXT = content.querySelector('#bigtext')
+        this.SMALLTEXT = content.querySelector('#smalltext')
+        this.ICON = content.querySelector('#icn')
+
+
+        shadowRoot.appendChild(linkElem);
+        shadowRoot.appendChild(content);
+
+    }
+
+    connectedCallback() {
+
+
+
+    }
+
+    static get observedAttributes() {
+        return ['bigtext', 'smalltext', 'icon'];
+    }
+
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'bigtext') {
+            // Update the element's content based on the new attribute value
+            this.BIGTEXT.innerHTML = newValue
+        }
+        if (name === 'smalltext') {
+            // Update the element's content based on the new attribute value
+            this.SMALLTEXT.innerHTML = newValue
+        }
+        if (name === 'icon') {
+            // Update the element's content based on the new attribute value
+            this.ICON.innerHTML = newValue
+        }
+    }
+
+
+}
+
+
+
+// Define the custom element
+window.customElements.define('bt-cardx1', btCardX1);
+
+
 
 
 
